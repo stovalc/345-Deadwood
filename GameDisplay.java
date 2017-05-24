14af294b097082cb424a44ba30d6f083c458c432
Grimzz <tmpurplehaze@gmail.com>
2017-05-24 15:38:22 -0700

    Add files via upload

--- GameDisplay.java
+++ GameDisplay.java
@@ -18,27 +18,61 @@ import javax.imageio.ImageIO;
 public class GameDisplay extends JFrame {
 	JPanel board;
 	JPanel info;
-	JLayeredPane layeredPane;
-    Dimension boardSize = new Dimension(1130, 850);
+	JLayeredPane background;
+    Dimension boardSize = new Dimension(1150, 850);
     Dimension paneSize = new Dimension(1300, 950);
     
     ImageIcon[] playerImages;
+    
+    static int numPlayers;
 	
 	public GameDisplay() {
-		layeredPane = new JLayeredPane();
-		getContentPane().add(layeredPane);
-		layeredPane.setPreferredSize(paneSize);
+		background = new JLayeredPane();
+		getContentPane().add(background);
+		background.setPreferredSize(paneSize);
 		
-		
+		//Board setup from different sections
 		board = new JPanel();
-		layeredPane.add(board, JLayeredPane.DEFAULT_LAYER);
+		background.add(board, JLayeredPane.DEFAULT_LAYER);
 		board.setLayout(new GridLayout(2, 2));
 		board.setPreferredSize(boardSize);
 		board.setBounds(0, 0, boardSize.width, boardSize.height);
 		String[] backgrounds = {"deadwoodboardstopleft.jpg", "deadwoodboardstopright.jpg", "deadwoodboardsbotleft.jpg", "deadwoodboardsbotright.jpg"};
 		for (int i = 0; i < 4; i++){
-			JLabel background = new JLabel(new ImageIcon(backgrounds[i]));
-			board.add(background);
+			JLabel backgroundImages = new JLabel(new ImageIcon(backgrounds[i]));
+			board.add(backgroundImages);
 		}
+		
+		//Adding buttons
+		JPanel buttons = new JPanel();
+		buttons.setLayout(new GridLayout(4, 1));
+		buttons.setBounds(0, boardSize.height, boardSize.width, 68);
+		JButton move = new JButton("Move");
+		move.setPreferredSize(new Dimension(40, 40));
+		JButton act = new JButton("Act");
+		act.setPreferredSize(new Dimension(40, 40));
+		JButton rehearse = new JButton("Rehearse");
+		rehearse.setPreferredSize(new Dimension(40, 40));		
+		JButton upgradeMoney = new JButton("Upgrade using money");
+		upgradeMoney.setPreferredSize(new Dimension(40, 40));
+		JButton upgradeCredits = new JButton("Upgrade using credits");
+		upgradeCredits.setPreferredSize(new Dimension(40, 40));
+		buttons.add(move);
+		buttons.add(act);
+		buttons.add(rehearse);
+		buttons.add(upgradeMoney);
+		buttons.add(upgradeCredits);
+		background.add(buttons, JLayeredPane.DEFAULT_LAYER);
 	}
+		
+		//Drop down options for number of players
+		public static int numOfPlayers(){
+			Object[] options = {"2", "3", "4", "5", "6", "7", "8"};
+			String pick = (String)JOptionPane.showInputDialog(null, "Please choose the number of players.\n", "Number of players?", JOptionPane.DEFAULT_OPTION, null, options, "0");
+			Integer numPlayers = Integer.parseInt(pick);
+			return numPlayers;		
+		}		
+		public int getNumOfPlayers(){
+			return numPlayers;
+		}		
 }
